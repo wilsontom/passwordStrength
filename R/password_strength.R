ffa5c1627b01a2e8b9ee14a46b5107ba9e0100a8
wilsontom <wilsontom89@gmail.com>
2019-09-18 12:33:22 +0100

    measure password strength

---
+++ R/password_strength.R
@@ -0,0 +1,38 @@
+#' Password Strength
+#'
+#' Test the strength of a password
+#'
+#' @param x a character string of the password to test
+#' @return a list of two elements
+#'  - ENTROPY; a numeric value for the password entropy
+#'  - STRENGTH; a strength rating of the password (Very Weak, Weak, Moderate, Strong, or Very Strong)
+#'
+#' @export
+
+password_strength <- function(x)
+{
+    CHARSET <- as.numeric(charset_size(x))
+
+    xsp <- strsplit(x, '')[[1]]
+
+    ENTROPY <- log2(CHARSET ^ nchar(x))
+
+
+    if (ENTROPY < 28) {
+        STRENGTH <- 'Very Weak'
+    }
+    if (ENTROPY >= 28 & ENTROPY <= 35) {
+        STRENGTH <- 'Weak'
+    }
+    if (ENTROPY >= 36 & ENTROPY <= 59) {
+        STRENGTH <- 'Moderate'
+    }
+    if (ENTROPY >= 60 & ENTROPY <= 127) {
+        STRENGTH <- 'Strong'
+    }
+    if (ENTROPY >= 128) {
+        STRENGTH <- 'Very Strong'
+    }
+
+    return(list(ENTROPY = ENTROPY, STRENGTH = STRENGTH))
+}
